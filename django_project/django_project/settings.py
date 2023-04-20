09d2a2bf7c6d312013b310259e73abf101194060
Yoojin93 <yoojinbae93@gmail.com>
2023-04-19 20:09:44 -0500

    update user profile

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
