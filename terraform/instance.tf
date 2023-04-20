9f2c26b74559272f69638bcc77d0458be10717eb
Yoojin93 <yoojinbae93@gmail.com>
2023-04-20 12:42:38 -0500

    updates to the instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "djangoblog_instance" {
   ami           = var.ami_id
-  instance_type = var.instance_type
-  count         = var.number_of_instances
+  instance_type = "${var.instance_type}"
+  count         = "${var.number_of_instances}"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name      = var.ami_key_pair_name
+  key_name      = "${var.ami_key_pair_name}"
+  vpc_security_group_ids = aws_security_group.django_sg.id
 
   tags = {
     Name = "Django Blog"
