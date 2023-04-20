2bfc63fd3afe2159a01b3091acf2d26bcbee2616
Yoojin93 <yoojinbae93@gmail.com>
2023-04-20 15:08:04 -0500

    alb

---
+++ terraform/alb.tf
@@ -0,0 +1,33 @@
+resource "aws_lb" "django_alb" {
+  name               = "DjangoBlog ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.webtier_sg.id]
+  subnets            = [aws_subnet.public_web_subnet.*.id]
+}
+
+resource "aws_lb_target_group" "django_target_group" {
+  name     = "Django target group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.djangoblog_vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "django_tg_attach" {
+  target_group_arn = aws_lb_target_group.django_target_group.arn
+  target_id        = aws_instance.djangoblog_instance.id
+  port             = 80
+depends_on = [
+    aws_instance.djangoblog_instance,
+  ]
+}
+
+resource "aws_lb_listener" "django_alb_listener" {
+  load_balancer_arn = aws_lb.django_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.django_target_group.arn
+  }
+}
